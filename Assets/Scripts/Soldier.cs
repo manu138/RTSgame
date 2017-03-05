4e6a947f70641f6a25bc49806d436e4b761b4539
Manuela Recio <manu138@hotmail.es>
2017-03-05 16:30:10 -0500

    no message

---
+++ Assets/Scripts/Soldier.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soldier : MonoBehaviour
+{
+    public int health;
+    public int defense;
+    public int attack;
+    public string team;
+
+    public void Attack()
+    {
+
+    }
+    
+    public void Move(float tiledistance)
+    {
+        tiledistance = 2.34f;
+        transform.position = new Vector2(transform.position.x + tiledistance, 0);
+    }
+
+    public void Stay()
+    {
+
+    }
+
+
+}
